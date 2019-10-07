ff8b1dac5926f9735a1b92d9641f48a2086cac7c
inuzm <inuzm@live.com>
2019-10-06 20:05:48 -0500

    added modification

--- Video_exp1/experiment2.R
+++ Video_exp1/experiment2.R
@@ -7,10 +7,10 @@ c1 <- 1; c2 <- -pi; c3 <- 1; c4 <- 1; c5 <- 1; c6 <- 3; c7 <- 1; c8 <- -1
 # Absolute values are not needed if the exponents remain constant and integer valued
 chale <- function(x, y){
     (
-        (c1 * abs(x)^e1 + c2 *  abs(y)^e2) *
-            exp(sign( cos(c3 * abs(x)^e3) * sin(c4 * abs(y)^e4) )) *
-            sin(sqrt(c5* abs(x)^e5 + c6 * abs(y)^e6)) *
-            cos(c7 * abs(x)^e7 + c8 * abs(y)^e8)
+        (c1 * x^e1 + c2 *  y^e2) *
+            exp(sign( cos(c3 * x^e3) * sin(c4 * y^e4) )) *
+            sin(sqrt(c5* x^e5 + c6 * y^e6)) *
+            cos(c7 * x^e7 + c8 * y^e8)
     ) %% 50.47
 }
 
@@ -28,7 +28,7 @@ require(colorspace)
 # Print the contour set and the level set (as image) as png-files of 800 × 800 with 100 ppi resolution
 # Then we modify the constants (ensuring the exponents to be greater than one which I think is useless right now)
 # Print which iteration has been done
-for(k in 1:100){
+for(k in 1:300){
     # %0nd ensures the file to have n leading zeroes (n should be the digits of the maximum number of iterations)
     png(sprintf("output-directory/%03d_bw.png", k), width = 800, height = 800, res = 100)
     par(mar = c(0,0,0,0) + 0.1)
@@ -41,24 +41,25 @@ for(k in 1:100){
     dev.off()
 
     # Comment whichever is not needed (I'm commenting the exponents variation to modify the absolute value)
-    # The modification is done via steps distributed as gaussian random variables with variance 1e-2 (as the
-    # value goes higher the difference between iterations will increase, probably 1e-3 is ok)
-    e1 <- max(e1 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e2 <- max(e2 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e3 <- max(e3 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e4 <- max(e4 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e5 <- max(e5 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e6 <- max(e6 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e7 <- max(e7 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    e8 <- max(e8 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
-    c1 <- c1 + rnorm(n = 1, sd = sqrt(1e-2))
-    c2 <- c2 + rnorm(n = 1, sd = sqrt(1e-2))
-    c3 <- c3 + rnorm(n = 1, sd = sqrt(1e-2))
-    c4 <- c4 + rnorm(n = 1, sd = sqrt(1e-2))
-    c5 <- max(c5 + rnorm(n = 1, sd = sqrt(1e-2)), 0)
-    c6 <- max(c6 + rnorm(n = 1, sd = sqrt(1e-2)), 0)
-    c7 <- c7 + rnorm(n = 1, sd = sqrt(1e-2))
-    c8 <- c8 + rnorm(n = 1, sd = sqrt(1e-2))
+    # The modification is done via steps distributed as gaussian random variables with variance 1e-3 (as the
+    # value goes higher the difference between iterations will increase, probably 1e-3 is ok, depending
+    # on the number of frames per second and whatever)
+    #e1 <- max(e1 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e2 <- max(e2 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e3 <- max(e3 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e4 <- max(e4 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e5 <- max(e5 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e6 <- max(e6 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e7 <- max(e7 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    #e8 <- max(e8 + rnorm(n = 1, sd = sqrt(1e-2)), 1)
+    c1 <- c1 + rnorm(n = 1, sd = sqrt(1e-3))
+    c2 <- c2 + rnorm(n = 1, sd = sqrt(1e-3))
+    c3 <- c3 + rnorm(n = 1, sd = sqrt(1e-3))
+    c4 <- c4 + rnorm(n = 1, sd = sqrt(1e-3))
+    c5 <- max(c5 + rnorm(n = 1, sd = sqrt(1e-3)), 0)
+    c6 <- max(c6 + rnorm(n = 1, sd = sqrt(1e-3)), 0)
+    c7 <- c7 + rnorm(n = 1, sd = sqrt(1e-3))
+    c8 <- c8 + rnorm(n = 1, sd = sqrt(1e-3))
 
     cat(sprintf("iteración %3d\n", k))
 }
