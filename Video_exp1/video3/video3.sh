358579ef89a0a9802c8eefbcd63c017a1d723fd5
inuzm <inuzm@live.com>
2019-11-01 16:58:10 -0600

    Error de frames en tercer video

--- Video_exp1/video3/video3.sh
+++ Video_exp1/video3/video3.sh
@@ -4,15 +4,15 @@ mkdir ./images
 
 # First argument is directory where images will be saved
 
-Rscript --vanilla ./video3.R ./images 10 20
+Rscript --vanilla ./video3.R ./images 60 30
 
 cd ./images
 
 # Black and white video
-ffmpeg -framerate 20 -i %04d_bw.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ../bw.mp4
+ffmpeg -framerate 30 -i %04d_bw.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ../bw.mp4
 
 # Color video
-ffmpeg -framerate 20 -i %04d_col.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ../col.mp4
+ffmpeg -framerate 30 -i %04d_col.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ../col.mp4
 
 cd ..
 rm -r ./images
